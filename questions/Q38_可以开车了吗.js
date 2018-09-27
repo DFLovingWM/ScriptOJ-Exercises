7f65c20855c184482a6b45f400ffca4076d8f7f8
大大大肥凯 <dafeikai@gmail.com>
2018-09-27 09:56:05 +0800

    修改Q38的Promise解法

--- questions/Q38_可以开车了吗.js
+++ questions/Q38_可以开车了吗.js
@@ -1,19 +1,39 @@
 var drive = console.log.bind(console)
 
 /**
- * 每个异步任务都判断是否该结束了
- *（该题目也许只有这种方法了，但拉下去有思考部分）
+ * 方法1：
+ * 每个异步任务执行结束后，判断一下是否所有任务都结束了
  */
 var driveCustomers = (...fns) => {
   let names = []
   for (const fn of fns) {
     fn((name) => {
-      names.push(name)
-      if (names.length === fns.length) {
+      names.push(name) // 执行本任务
+      if (names.length === fns.length) { // 判断是否所有任务都结束了
         drive(names)
       }
     })
   }
+}
+
+/**
+ * 方法2：利用Promise + async/await来控制异步(并发)流程
+ * 每一个人相当于一个异步任务，最后开车的步骤需要在每个异步任务都完成了之后才进行，所以这其实是一种异步流程控制。
+ * 所以方法1是不合理的，因为“是否要开车”这个任务，是独立于任何一个异步任务之外的，按理来说不应该由每一个都进行判断。
+ * 故要借助Promise.all来对单个任务、全局任务解耦。
+ */
+var driveCustomers = async (...fns) => {
+  let names = [] // 保存结果，对于每个异步任务来说是"全局"变量
+  let promises = fns.map(fn => {
+    return new Promise((resolve, reject) => {
+      fn((name) => {
+        names.push(name)
+        resolve() // 这里调用resolve，表明本任务结束(不必传参)
+      })
+    })
+  })
+  await Promise.all(promises)
+  drive(names) // 到这一行时，说明所有任务已经结束，这时候可以"开车"了
 }
 
 /**
@@ -38,59 +58,3 @@ var bb = (callback) => {
 }
 
 driveCustomers(jk, cl, bb)
-
-
-// =================================  以下是思(胡)考(扯)部分  ==================================
-
-/**
- * 思考：利用Promise + async/await来控制异步(并发)流程
- * 每一个人相当于一个异步任务，最后开车的步骤需要在每个异步任务都完成了之后才进行，
- * 所以这其实是一种异步流程控制。
- * 这样想的话，上面的方法是不合理的，因为“是否要开车”这个任务，是独立于任何一个异步任务之外的，按理来说不应该由每一个都进行判断(低耦合)。
- * 
- * 然而这种方法是行不通的，因为并发的Promise.all返回的是一个数组，分不清谁先谁后。所以是写着玩玩的，为了联系一下Promise的知识。
- */
-var driveCustomers = async (...fns) => {
-  let result = await Promise.all(fns.map(callback2Promise))
-  result = result.sort((a, b) => a.time - b.time).map(person => person.name)
-  drive(result)
-}
-// 将callback风格的函数转为Promise
-function callback2Promise (fn) {
-  return new Promise((resolve, reject) => {
-    fn(resolve)
-  })
-}
-
-/**
- * 测试代码
- */
-var jk = (callback) => {
-  setTimeout((time, name) => {
-    callback({
-      time,
-      name
-    })
-  }, 10, 10, 'JK') // 上车时间不一定
-}
-
-var cl = (callback) => {
-  setTimeout((time, name) => {
-    callback({
-      time,
-      name
-    })
-  }, 3, 3, 'CL') // 上车时间不一定
-}
-
-var bb = (callback) => {
-  setTimeout((time, name) => {
-    callback({
-      time,
-      name
-    })
-  }, 5, 5, 'BB') // 上车时间不一定
-}
-
-driveCustomers(jk, cl, bb)
-
--- questions/readme.txt
+++ questions/readme.txt
@@ -1,5 +1,6 @@
 好题目：
 Q33：数组flatten，探索N种方式
+Q38：开车，有一种方法可以联系"异步流程控制"
 Q52：中间件模式的实现，与Koa等Node框架有关
 Q57：shallowEqual的实现，与React的重渲染有关
 Q72：generator模拟async/await，实现一个co、认识其意义
